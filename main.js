3184165bbeb17482d3443ebefefdeef8035b35b1
L.Walker <logantwalker95@gmail.com>
2020-07-21 15:05:46 -0400

    api calls and rendering started

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
-//import { testQuery } from './modules/weatherData.js';
+import { initTab, querySubmit } from './modules/weatherData.js';
 
-function testQuery(){
-    const city='Atlanta';
-    const apiKey = '077020626c2d3cd50a785299625eb51a'
-    const queryURL = `api.openweathermap.org/data/2.5/weather?q=London,uk&appid=077020626c2d3cd50a785299625eb51a`
-    
-    const onAccept = (data) => {
-        console.log(data);
-    }
-    const onReject = (errThrown) =>{
-        console.log(errThrown);
-    }
+initTab();
 
-    $.ajax({
-        method:'GET',
-        url: queryURL
-    }).then(onAccept, onReject);
-}
+$('#search').on('change',querySubmit);
 
 
-testQuery();
 //initializing materialize css styling
-$(document).ready(function(){
+$(document).ready(function () {
     $('.tabs').tabs();
-  });
+});
--- modules/weatherData.js
+++ modules/weatherData.js
@@ -1,15 +1,80 @@
+export function initTab(){
 
-export function testQuery(){
-    const city='atlanta';
-    const state='ga';
-    const apiKey = '077020626c2d3cd50a785299625eb51a'
-    const queryURL = `api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}`
-    
+    const initCity = 'atlanta';
+    const initState = 'ga';
+    const initCountry='us';
+    const apiKey = '077020626c2d3cd50a785299625eb51a';
+    let queryURL = `api.openweathermap.org/data/2.5/weather?q=${initCity},${initState},${initCountry}&appid=${apiKey}`;
+
+    const onReject = (errThrown) => {
+        console.log(errThrown);
+        console.log(errThrown.responseText);
+    }
+
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                console.log(response);
-            });
+        url: `http://${queryURL}`
+    }).then(data =>{
+
+        $("#initLocation").text(data.name);
+
+    }, onReject);
+}
+
+export function querySubmit(){
+
+    formatInput()
+    
+}
+
+// class Event{
+//     constructor(eventTitle,startTime,endTime,description){
+//         this.title=eventTitle;
+//         this.startTime = startTime;
+//         this.endTime = endTime;
+//         this.description = description;
+//     }
+
+// class Location{
+//     constructor(){
+
+//     }
+// }
+
+function formatInput(){
+    let userInput = $('#search').val();
+    if(userInput.includes(', ')){
+        let rawInput = userInput.replace(/\s+/g,'');
+
+        let locationArr = rawInput.split(',');
+        let city = locationArr[0];
+        let state = locationArr[1];
+
+        searchApiCall(city,state);
+    }
+    else{
+        console.log('bad format');
+    }
+
+}
+
+
+function searchApiCall(city,state){
+
+    const country='us';
+    const apiKey = '077020626c2d3cd50a785299625eb51a';
+    let queryURL = `api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${apiKey}`;
+
+    const onReject = (errThrown) => {
+        console.log(errThrown);
+        console.log(errThrown.responseText);
+    }
+
+    $.ajax({
+        url: `http://${queryURL}`
+    }).then(data =>{
+        let newTab = `<li class="tab col"><a href="#initLocation">${data.name}</a></li>`;
+        $('.tabs').append(newTab);
+        
+
+    }, onReject);
 }
